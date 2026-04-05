4eadf4fc259e6127812efd0d03f881baf0b13695
SasiKumar-08 <sasikumarreddyyenuga@gmail.com>
2026-04-05 22:03:38 +0530

    fixed env

--- database.py
+++ database.py
@@ -6,10 +6,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Get DB URL from .env
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+# If not set → use SQLite (for deployment)
+if not DATABASE_URL:
+    DATABASE_URL = "sqlite:///./test.db"
 
-SessionLocal = sessionmaker(bind=engine)
+# SQLite specific setting
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
 
+# Create engine
+engine = create_engine(
+    DATABASE_URL,
+    connect_args=connect_args
+)
+
+# Session
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+# Base
 Base = declarative_base()
